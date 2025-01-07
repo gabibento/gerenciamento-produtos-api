713958b55e5239a18d20b29a62a124b3f3035ef2
Gabriella <gabriella.mabento@gmail.com>
2025-01-07 14:20:58 -0300

    refactor: modify attribute email to username in DTOs

--- gerenciador/src/main/java/com/produtos/gerenciador/entities/user/AuthenticationDTO.java
+++ gerenciador/src/main/java/com/produtos/gerenciador/entities/user/AuthenticationDTO.java
@@ -1,4 +1,4 @@
 package com.produtos.gerenciador.entities.user;
 
-public record AuthenticationDTO(String login, String password) {
+public record AuthenticationDTO(String username, String password) {
 }
--- gerenciador/src/main/java/com/produtos/gerenciador/entities/user/RegisterDTO.java
+++ gerenciador/src/main/java/com/produtos/gerenciador/entities/user/RegisterDTO.java
@@ -1,4 +1,4 @@
 package com.produtos.gerenciador.entities.user;
 
-public record RegisterDTO(String login, String password, String role) {
+public record RegisterDTO(String username, String password, String role) {
 }
