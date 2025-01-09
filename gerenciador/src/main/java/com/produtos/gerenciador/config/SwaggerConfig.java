bc96230c93958db7f0f544bbbba25a33a6ff5b54
Gabriella <gabriella.mabento@gmail.com>
2025-01-08 22:19:40 -0300

    fix: config security in SwaggerConfig to enable authentication

--- gerenciador/src/main/java/com/produtos/gerenciador/config/SwaggerConfig.java
+++ gerenciador/src/main/java/com/produtos/gerenciador/config/SwaggerConfig.java
@@ -1,18 +1,20 @@
 package com.produtos.gerenciador.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("Product API")
-                        .description("API for managing products")
-                        .version("1.0"));
-    }
 }
